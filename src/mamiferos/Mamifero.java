938f7798a7c3ec940873072f8258e57d10fe2548
Edson Costa <edson.13.10.2004@gmail.com>
2023-09-11 13:33:57 -0300

    🌟 feat: Class Mamifero add

---
+++ src/mamiferos/Mamifero.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mamiferos;
+
+/**
+ *
+ * @author edson
+ */
+public class Mamifero {
+    private String nome;
+    private int idade;
+    private String raça;
+
+
+    public String comer() {
+        return(nome + " está comendo.");
+    }
+
+    public String dormir() {
+        return(nome + " está dormindo.");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRaça() {
+        return raça;
+    }
+
+    public void setRaça(String raça) {
+        this.raça = raça;
+    }
+}
+
